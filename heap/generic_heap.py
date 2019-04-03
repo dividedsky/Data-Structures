0fc4db89ec4fc7f0b5deddbd7ef38e3f32166572
Justin <jdlow2@gmail.com>
2019-04-03 11:31:18 -0400

    completed generic heap

--- heap/generic_heap.py
+++ heap/generic_heap.py
@@ -1,26 +1,22 @@
 class Heap:
-    def __init__(self, comparator=max):
+    def __init__(self, comparator=lambda x, y: x > y):
         self.storage = []
         self.comparator = comparator
 
     def insert(self, value):
         self.storage.append(value)
         self._bubble_up(len(self.storage) - 1)
-        print("after insert:", self.storage)
 
     def delete(self):
-        print("before delete:", self.storage)
         if len(self.storage) > 0:
             val = self.storage.pop(0)
             if len(self.storage):
                 end = self.storage.pop()
                 self.storage.insert(0, end)
                 self._sift_down(0)
-            print("after delete:", self.storage)
             return val
 
     def get_priority(self):
-        print(self.storage)
         return self.storage[0]
 
     def get_size(self):
@@ -29,10 +25,7 @@ class Heap:
     def _bubble_up(self, index):
         if index > 0:
             parent_idx = (index - 1) // 2
-            if (
-                self.comparator(self.storage[index], self.storage[parent_idx])
-                == self.storage[index]
-            ):
+            if self.comparator(self.storage[index], self.storage[parent_idx]):
                 self.storage[index], self.storage[parent_idx] = (
                     self.storage[parent_idx],
                     self.storage[index],
@@ -52,23 +45,13 @@ class Heap:
             right = None
         if not left and not right:
             return None  # return if we are at a leaf
-        if not right or self.comparator(left, right) == left:
+        if not right or self.comparator(left, right):
             priority_idx = left_idx
             priority_val = left
-            # self.storage[index], self.storage[left_idx] = (
-            #     self.storage[left_idx],
-            #     self.storage[index],
-            # )
-            # self._sift_down(left_idx)
         else:
             priority_idx = right_idx
             priority_val = right
-            # self.storage[index], self.storage[right_idx] = (
-            #     self.storage[right_idx],
-            #     self.storage[index],
-            # )
-            # self._sift_down(right_idx)
-        if self.comparator(self.storage[index], priority_val) == priority_val:
+        if self.comparator(priority_val, self.storage[index]):
             self.storage[index], self.storage[priority_idx] = (
                 self.storage[priority_idx],
                 self.storage[index],
